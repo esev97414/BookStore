9099a9e9ffcb2619031139d500498178e87a47aa
esev97414 <55944042+esev97414@users.noreply.github.com>
2019-10-01 02:26:32 +0200

    Delete ICustomerPromotionStrategy.cs

--- BookStore/Interfaces/ICustomerPromotionStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BookStore.Interfaces
-{
-    public interface ICustomerPromotionStrategy
-    {
-        double CalculatePromotion(List<string> list);
-    }
-}
